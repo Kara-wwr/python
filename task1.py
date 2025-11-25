32104ea7cd42fed62ac792e7c75ecccc842c8d7a
Kara_WWr <162871136+Kara-wwr@users.noreply.github.com>
2025-11-25 14:26:25 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -43,3 +43,45 @@ path = 'C:\\Games\\city simulator'
 print(path)
 error_path = path * 2
 print('error path:', error_path)
+
+               #3 задание
+num_1 = 7 
+num_2 = 10
+num_3 = 4
+
+summ = num_1 + num_2 + num_3
+print('Сумма всех чисел:',summ)\
+
+
+num_1 = 7.9 
+num_2 = 21.3
+num_3 = float(4)
+
+summ = num_1 + num_2 + num_3
+print('Сумма всех нецелых чисел:', summ)
+
+
+num_1 = 130
+num_2 = 4
+num_3 = 2
+
+multiplayng = num_1 * num_2 * num_3
+print("Произведение вех чисел:", multiplayng)
+division = (num_1 / num_2) / num_3
+print('Деление:', division)
+ 
+
+num_1 = 2
+num_2 = 3
+num_3 = 4
+
+degree = (num_1 ** num_2) ** num_3
+print ('Числап в степени', degree)
+
+
+num_1 = 2
+num_2 = 8
+num_3 = 5
+
+math = ((43 = num_1)+(num_2 + 67)-(num_3 * 2))//2
+print(math)
